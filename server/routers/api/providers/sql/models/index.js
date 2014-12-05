2fec28315f9ed344cea32693855cdfb1d4fe02fc
Martin Di Domenico <martin.didomenico@olx.com>
2014-12-05 19:48:39 -0300

    PROVIDERS :: Sql
    Improved Sql provider

--- server/routers/api/providers/sql/models/index.js
+++ server/routers/api/providers/sql/models/index.js
@@ -4,8 +4,8 @@ var Key = require('./key');
 var Metric = require('./metric');
 
 module.exports = function Models(database) {
-    this.Key = new Key(database);
-    this.Metric = new Metric(database);
-    this.Key.hasMany(this.Metric);
+    this.key = new Key(database);
+    this.metric = new Metric(database);
+    this.key.hasMany(this.metric);
     database.sync();
 };
